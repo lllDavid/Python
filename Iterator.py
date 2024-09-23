6e522697d2b96d0ffb94793d6e60f8947b540fcc
lllDavid <imperfectrising5527@gmail.com>
2024-09-23 14:54:28 +0200

    sync

--- Iterator.py
+++ Iterator.py
@@ -36,7 +36,7 @@ for quadrat in quadrierteZahlen:
 class iter:
     def __iter__(self):
         self.num = 1
-        self.wert = int(input("Range: "))
+        self.wert = int(input("Enter range: "))
         return self
     def __next__(self):
         if self.num <= self.wert:
--- Lambda.py
+++ Lambda.py
@@ -8,7 +8,7 @@ def doubleNumbers(wert):
     double = lambda x : x * 2
     doubledNumber = double(wert)
     print(doubledNumber)
-doubleNumbers((int(input("Geben sie eine Zahl zum verdoppeln an: "))))
+doubleNumbers((int(input("Geben sie eine Zahl zum verdoppeln ein: "))))
 
 #3)
 from itertools import groupby
