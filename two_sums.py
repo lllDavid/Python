3a587b06131c03378fe5f75a573f560bc47f8e88
lllDavid <imperfectrising5527@gmail.com>
2024-09-24 18:08:46 +0200

    sync

--- two_sums.py
+++
@@ -1,9 +0,0 @@
-def twoSum(numbers, target):
-    complement = {}
-    for i, num in enumerate(numbers):
-        if target - num in complement:
-            return [complement[target - num], i]
-        complement[num] = i
-numbers = [2,3,4,6]
-target = 7
-print(twoSum(numbers, target))
