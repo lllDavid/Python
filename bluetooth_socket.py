96d0dc55ca2245c3f83bf2321c0a82ca96e88ab0
lllDavid <imperfectrising5527@gmail.com>
2024-10-18 18:14:55 +0200

    sync

--- bluetooth_socket.py
+++ bluetooth_socket.py
@@ -3,5 +3,14 @@ import socket
 addr = ""
 port = ""
 
-b_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-b_socket.connect(addr,port)
+def connect():
+    b_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    b_socket.bind(addr,port)
+    b_socket.listen(1)
+    print("Ready to receive data")
+    while True:
+        conn, addr = b_socket.accept()
+
+connect()
+
+
