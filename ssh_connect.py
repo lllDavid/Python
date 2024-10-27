16dd3aba1deb3b3bacbca3899a6486f162c619f5
lllDavid <imperfectrising5527@gmail.com>
2024-10-27 13:47:40 +0100

    sync

--- ssh_connect.py
+++ ssh_connect.py
@@ -1,14 +1,11 @@
 import subprocess
 
-# Define server and user information
-hostname = 'your.server.com'  # Replace with your server's hostname or IP address
-username = 'your_username'      # Replace with your SSH username
+hostname = ''  
+username = ''      
 
-# Construct the SSH command
 ssh_command = f"ssh {username}@{hostname}"
 
 try:
-    # Execute the SSH command
     subprocess.run(ssh_command, shell=True)
 except Exception as e:
     print(f"Failed to execute SSH command: {e}")
