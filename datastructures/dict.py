5afcde9d4bb1a8f9ba9a89b53741e4a23d7b40ab
lllDavid <imperfectrising5527@gmail.com>
2025-02-09 13:23:22 +0100

    sync

---
+++ datastructures/dict.py
@@ -0,0 +1,13 @@
+dict1 = {}
+
+username = "David"
+email = "david@gmail.com"
+password = 123456
+
+dict1.update({"Username":username})
+
+dict1.update({"Email":email})
+
+dict1.update({"Password":password})
+
+print(dict1)
