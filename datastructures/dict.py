1ec0fbce1a70b928b2d93362ade3e1c578396406
lllDavid <imperfectrising5527@gmail.com>
2025-02-09 13:33:41 +0100

    sync

--- datastructures/dict.py
+++ datastructures/dict.py
@@ -4,12 +4,34 @@ password = 123456
 
 # Create
 dict1 = {"Username:":username, "Email":email, "Password:":password}
+
 # Update
 age = 23
 dict1.update({"Age":age})
+
 # Print
 print(dict1)
-# Print
+
+# Access value by key
+print(dict1["Age"])
+
+# Get all keys
+keys = dict1.keys()
+print(keys)
+
+# Get all values
+values = dict1.values()
+print(values)  
+
+# Get all keys and values 
 for key, value in dict1.items():
     print(f"Key: {key}, Value: {value}")
 
+# Delete
+dict1.pop("Email")
+print(dict1)
+
+# Dictionary comprehension
+dict1 = {"1":1, "2":2, "3":3}
+dict2 = { v + 1 for k,v in dict1.items() if v > 2}
+print(dict2)
