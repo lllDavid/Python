d01e016de92f8d06080062460d38e844bb597f75
lllDavid <imperfectrising5527@gmail.com>
2025-02-13 15:05:06 +0100

    sync

--- datastructures/binary_tree/binary_tree_test.py
+++ datastructures/binary_tree/binary_tree_test.py
@@ -1,11 +1,11 @@
 class Node:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
     
     def insert(self, value):
-        if value < self.root:
+        if value < self.value:
             if self.left is None:
                 self.left = Node(value)
             else:
@@ -15,3 +15,26 @@ class Node:
                 self.right = Node(value)
             else:
                 self.right.insert(value)
+
+    def inorder_traversal(self):
+        if self.left:
+            self.left.inorder_traversal()
+        print(self.value)
+        if self.right:
+            self.right.inorder_traversal()
+
+    
+node = Node(6)
+node.insert(5)
+node.insert(2)
+node.insert(4)
+node.insert(1)
+node.insert(2)
+node.insert(4)
+node.insert(19)
+node.insert(29)
+node.insert(11)
+node.insert(4)
+node.insert(2)
+
+node.inorder_traversal()
