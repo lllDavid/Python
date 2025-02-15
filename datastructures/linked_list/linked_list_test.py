6b5723ae2ad993d6a48fb5a9ad78e3a51c343517
lllDavid <imperfectrising5527@gmail.com>
2025-02-15 12:01:51 +0100

    sync

--- datastructures/linked_list/linked_list_test.py
+++ datastructures/linked_list/linked_list_test.py
@@ -1,1 +1,15 @@
 class Node:
+    def __init__(self, data):
+        self.data  = data
+        self.next = None
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next 
+                current.next = new_node
