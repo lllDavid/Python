8299f403421b359e5ffc8ae0cf0d372b3afcae7b
lllDavid <imperfectrising5527@gmail.com>
2025-03-08 12:59:46 +0100

    sync

--- strings.py
+++ strings.py
@@ -9,3 +9,21 @@ print(text.translate(trans))  # Output: h2ll4
 trans = str.maketrans("", "", "aeiou")
 text = "hello world"
 print(text.translate(trans))  # Output: hll wrld
+
+# str.center(width, fillchar)
+"hello"
+print(text.center(10, "-"))  # Output: "--hello---"
+
+# str.lstrip([chars]
+text = "   hello"
+print(text.lstrip())  # Output: "hello"
+
+text = "xxxyhello"
+print(text.lstrip("xy"))  # Output: "hello" (removes 'x' and 'y')
+
+#str.isspace()
+text = "   "
+print(text.isspace())  # Output: True
+
+text = "hello"
+print(text.isspace())  # Output: False
