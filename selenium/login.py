3800900a01b5aa574bdc65f36531f13ae1264668
lllDavid <imperfectrising5527@gmail.com>
2025-03-11 02:48:31 +0100

    sync

---
+++ selenium/login.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from time import sleep
+
+driver = webdriver.Chrome()
+
+driver.get("http://127.0.0.1:5000/login")  
+
+username_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "username"))
+)
+
+password_field = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "password"))
+)
+
+username_field.send_keys("")  
+password_field.send_keys("")  
+
+password_field.send_keys(Keys.RETURN)
+
+sleep(3) 
+
+driver.quit()
