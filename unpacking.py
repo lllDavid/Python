f4c0275e3ab579d94088bf8257eecdb831154352
lllDavid <imperfectrising5527@gmail.com>
2025-03-13 15:31:05 +0100

    sync

---
+++ unpacking.py
@@ -0,0 +1,20 @@
+# dict
+person = {"name": "Alice", "age": 30, "city": "New York"}
+
+{name, city} = {**person}  
+print(f"name={name}, city={city}")
+
+# merge
+dict1 = {"a": 1, "b": 2}
+dict2 = {"c": 3, "d": 4}
+
+merged_dict = {**dict1, **dict2}
+print(f"merged_dict={merged_dict}")
+
+# func
+def function(*args, **kwargs):
+    print("Positional arguments:", args)
+    print("Keyword arguments:", kwargs)
+
+function(1, 2, 3, name="Alice", age=30)
+
