772b75b5ad0cda62ea9e9ce0cda8b9c12896c8e4
lllDavid <imperfectrising5527@gmail.com>
2025-03-20 13:57:13 +0100

    sync

---
+++ algorithms/search/breadth_search.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=' ')  
+            visited.add(node)
+            queue.extend(graph[node] - visited)  
+
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F', 'G'},
+    'D': {'B'},
+    'E': {'B', 'H'},
+    'F': {'C'},
+    'G': {'C'},
+    'H': {'E'}
+}
+
+bfs(graph, 'A')
--- multithreading.py
+++ multithreading.py
@@ -1,4 +1,3 @@
-import threading
 import time
 import random
 from concurrent.futures import ThreadPoolExecutor
