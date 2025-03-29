29c570ec97b224bd08c0733bc5abaf9fa0799825
lllDavid <imperfectrising5527@gmail.com>
2025-03-29 16:58:53 +0100

    sync

---
+++ datastructures/linked_list/reversed.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.value, end=" -> ")
+            current = current.next
+        print("None")
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next  
+            current.next = prev       
+            prev = current            
+            current = next_node       
+        self.head = prev  
