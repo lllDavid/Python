0b8a71f6d74ea069875e88633301ece99a18d1b4
lllDavid <imperfectrising5527@gmail.com>
2025-04-01 14:45:02 +0200

    sync

---
+++ datastructures/array.py
@@ -0,0 +1,11 @@
+import array
+
+arr = array.array('i', [1, 2, 3, 4, 5])
+
+print(arr[0])  
+
+arr[1] = 10
+print(arr)  
+
+arr.append(6)
+print(arr) 
---
+++ datastructures/counter.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+data = ['apple', 'banana', 'apple', 'orange', 'banana', 'banana']
+counter = Counter(data)
+
+print(counter)  
+
+text = "hello world"
+char_counter = Counter(text)
+
+print(char_counter) 
