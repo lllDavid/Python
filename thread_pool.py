7f06773678acc6970ef61102ec58b54e7479e1fc
lllDavid <imperfectrising5527@gmail.com>
2025-04-18 01:17:30 +0200

    sync

---
+++ thread_pool.py
@@ -0,0 +1,13 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+def fetch_data(x):
+    print(f"Fetching {x}")
+    time.sleep(1)
+    return f"Data {x}"
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    results = executor.map(fetch_data, [1, 2, 3, 4, 5])
+
+for res in results:
+    print(res)
